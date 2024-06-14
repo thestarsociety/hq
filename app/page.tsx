2c87f62f1ad3c84acf2cc12b3fcfead03e4eeb07
Surya <suryakant.subudhi@outlook.com>
2024-06-14 12:20:31 +0530

    feat: Added hero section in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+"use client"
+import RetroGrid from "@/components/magicui/retro-grid";
 
 export default function Home() {
   return (
-    <div>
-      TSS
+    <div className="flex relative h-screen w-full items-center justify-center overflow-hidden">
+      <RetroGrid />
+      <div className="z-10 text-7xl font-bold text-center pointer-events-none">
+        <h1>The Star Society</h1>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        meteor: "meteor 5s linear infinite",
+        grid: "grid 15s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
+        grid: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
