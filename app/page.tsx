4517b9f9499b1078b080c23093264f9cc6a83de7
Surya <suryakant.subudhi@outlook.com>
2024-06-15 12:17:15 +0530

    feat: added conditional logos for different themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 "use client"
+import { TssDarkIcon } from "@/components/icons/tssDarkLogo";
 import { TssIcon } from "@/components/icons/tssLogo";
 import RetroGrid from "@/components/magicui/retro-grid";
 import ShimmerButton from "@/components/magicui/shimmer-button";
 import TypingAnimation from "@/components/magicui/typing-animation";
+import { ModeToggle } from "@/components/modeToggle";
+import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const { theme, systemTheme } = useTheme();
+  const [resolvedTheme, setResolvedTheme] = useState(theme);
+
+  useEffect(() => {
+    if(theme === "system") {
+      setResolvedTheme(systemTheme);
+    } else {
+      setResolvedTheme(theme);
+    }
+  }, [theme, systemTheme]);
 
   const router = useRouter();
 
@@ -16,9 +31,13 @@ export default function Home() {
   return (
     <div className="flex relative h-screen w-full items-center justify-center overflow-hidden">
       <div className="z-10 text-center flex flex-col items-center justify-center space-y-5">
-        <TssIcon height={250} width={250}/>
+        {resolvedTheme === "dark" ? (
+          <TssIcon height={250} width={250}/>
+        ) : (
+          <TssDarkIcon height={250} width={250}/>
+        )}
         <TypingAnimation 
-          className="text-xl font-semibold text-white"
+          className="text-xl font-semibold text-black dark:text-white"
           text="We build cool stuff"
           duration={200}
         />
@@ -29,6 +48,9 @@ export default function Home() {
         </ShimmerButton>
       </div>
       <RetroGrid />
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
     </div>
   );
 }
