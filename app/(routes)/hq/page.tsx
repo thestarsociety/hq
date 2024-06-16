a62b5a9aa284b458976d8ade602fcd021cc5eab3
Surya <suryakant.subudhi@outlook.com>
2024-06-16 21:38:13 +0530

    feat: temp community

--- app/(routes)/hq/page.tsx
+++ app/(routes)/hq/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Boxes } from "@/components/magicui/boxes";
+import { ShinyButton } from "@/components/magicui/shiny-button";
 import SparklesText from "@/components/magicui/sparckles-text";
 
 const HqPage = () => {
@@ -7,10 +8,11 @@ const HqPage = () => {
         <div className="flex relative h-screen w-full items-center justify-center overflow-hidden">
             <div className="z-10 text-center flex flex-col items-center justify-center space-y-5">
                 <SparklesText
-                    text="Coming Soon"
+                    text="Join community"
                     className="text-7xl font-bold"
                     colors={{ first: "#FF930F", second: "#FFF95B" }}
                 />
+                <ShinyButton name="Discord" urlPush="https://discord.gg/eyySD7jCh7"/>
             </div>
             <Boxes />
         </div>
